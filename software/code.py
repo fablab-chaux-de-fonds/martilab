6f5e444a69718be9cb56cd9ea3daacf6ff33e329
Raphaël Monnard <raphael@monnard.email>
2024-04-24 21:35:14 +0200

    remove deep sleep

--- software/code.py
+++ software/code.py
@@ -9,10 +9,12 @@ from DFPlayer import DFPlayer  # Importer la classe DFPlayer depuis un fichier e
 
 # Définir l'heure après avoir changé la pile.
 # Décommentez la ligne suivante et réglez l'heure actuelle
-init_time = time.struct_time((2024, 4, 22, 17, 22, 0, 0, -1, -1))
+# init_time = time.struct_time((2024, 4, 24, 21, 32, 30, 0, -1, -1))
 
 # Volume du lecteur
-volume = 40
+volume = 100
+
+is_play = False
 
 def log(message):
    try:
@@ -42,38 +44,42 @@ rtc = adafruit_ds1307.DS1307(i2c)
 
 # Si 'init_time' est défini, définir l'heure initiale du RTC
 if 'init_time' in locals():
+    print(str(rtc.datetime) + "Init RTC")
+    log("Init RTC")
     rtc.datetime = init_time
+    
+# Initialiser le DFPlayer
+df_player_pin = digitalio.DigitalInOut(board.GP15)  # Broche pour le contrôle du lecteur DFPlayer
+df_player_pin.direction = digitalio.Direction.OUTPUT  # Définir la direction de la broche comme sortie
+df_player_pin.value = False  # Initialiser la valeur de la broche à False
+time.sleep(3)  # Attendre 5 secondes pour laisser le DFPlayer initialiser
 
-# Vérifier si l'heure actuelle est entre 08:00 et 10:00 ou entre 12:00 et 14:00 ou entre 18:00 et 21:00
-if 8 <= rtc.datetime.tm_hour <= 9 or 12 <= rtc.datetime.tm_hour < 14 or 18 <= rtc.datetime.tm_hour < 21:
-    # print("Démarrage du lecteur")
+dfplayer = DFPlayer(volume=volume)  # Créer une instance de la classe DFPlayer avec le volume spécifié
+print(str(rtc.datetime) + "dfplayer initialized")
+log("dfplayer initialized")
 
-    # Initialiser le DFPlayer
-    df_player_pin = digitalio.DigitalInOut(board.GP15)  # Broche pour le contrôle du lecteur DFPlayer
-    df_player_pin.direction = digitalio.Direction.OUTPUT  # Définir la direction de la broche comme sortie
-    df_player_pin.value = False  # Initialiser la valeur de la broche à False
-    time.sleep(5)  # Attendre 5 secondes pour laisser le DFPlayer initialiser
+while True:
+    # Vérifier si l'heure actuelle est entre 08:00 et 10:00 ou entre 12:00 et 14:00 ou entre 18:00 et 21:00
+    if 8 <= rtc.datetime.tm_hour <= 9 or 12 <= rtc.datetime.tm_hour < 14 or 18 <= rtc.datetime.tm_hour < 22:
 
-    dfplayer = DFPlayer(volume=volume)  # Créer une instance de la classe DFPlayer avec le volume spécifié
+        if not is_play:
+            log("Début de la lecture")
+            print(str(rtc.datetime) + "Début de la lecture")
+        dfplayer.play()  # Commencer la lecture
+        is_play = True
+        
+        # print('Éteindre le lecteur DFPlayer')
+        df_player_pin.direction = digitalio.Direction.INPUT  # Éteindre le lecteur DFPlayer
 
-    # print('Allumer le lecteur DFPlayer')
-    df_player_pin.direction = digitalio.Direction.OUTPUT  # Allumer le lecteur DFPlayer
-    time.sleep(1)  # Attendre 1 seconde
-    log("Début de la lecture")
-    dfplayer.loop()  # Commencer la lecture
-    time.sleep(30 * 60 * 2)  # Attendre 2h (durée de lecture du DFPlayer)
-    log("Fin de la lecture")
-
-    # print('Éteindre le lecteur DFPlayer')
-    df_player_pin.direction = digitalio.Direction.INPUT  # Éteindre le lecteur DFPlayer
-
-    # Dormir pendant 2 heures
-    time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 60 * 60 * 2)
-    alarm.exit_and_deep_sleep_until_alarms(time_alarm)
-
-else:
-    # Si l'heure actuelle n'est pas dans la plage spécifiée, attendre 15 minutes
-    # print("Attendre 15 minutes")
-    log("Attendre 15 minutes")
-    time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 900)
-    alarm.exit_and_deep_sleep_until_alarms(time_alarm)
+    else:
+        # Si l'heure actuelle n'est pas dans la plage spécifiée, attendre 15 minutes
+        # print("Attendre 15 minutes")
+        if is_play:
+            print(str(rtc.datetime) + "Sleep mode")
+            log("Sleep mode")
+        dfplayer.stop()
+        is_play=False
+        
+        #time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 900)
+        time.sleep(10)
+        
